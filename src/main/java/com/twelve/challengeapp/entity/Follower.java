dd4f58f890d0a4a47cb8b55e985e020651decd87
Hyungs0703 <hyungs0703@naver.com>
2024-07-03 14:48:13 +0900

    produce Follower

---
+++ src/main/java/com/twelve/challengeapp/entity/Follower.java
@@ -0,0 +1,31 @@
+package com.twelve.challengeapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+@MappedSuperclass
+public class Follower {
+
+    @Getter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+
+}
