e668db608b701cb3e0c9917d3e49c5b7624acf9e
Hyungs0703 <hyungs0703@naver.com>
2024-07-04 21:16:39 +0900

    댓글 좋아요 실패 테스트 코드 구현

--- src/test/java/com/twelve/challengeapp/service/like/CommentLikeServiceImplTest.java
+++ src/test/java/com/twelve/challengeapp/service/like/CommentLikeServiceImplTest.java
@@ -2,13 +2,17 @@ package com.twelve.challengeapp.service.like;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
 
 import com.twelve.challengeapp.entity.Comment;
 import com.twelve.challengeapp.entity.Post;
 import com.twelve.challengeapp.entity.User;
 import com.twelve.challengeapp.entity.like.CommentLike;
+import com.twelve.challengeapp.exception.CommentNotFoundException;
 import com.twelve.challengeapp.jwt.UserDetailsImpl;
 import com.twelve.challengeapp.repository.CommentRepository;
 import com.twelve.challengeapp.repository.UserRepository;
@@ -64,29 +68,22 @@ class CommentLikeServiceImplTest {
             .commentLikes(new HashSet<>())
             .user(userDetails.getUser())
             .build();
-
-
     }
 
     @Test
     @DisplayName("댓글 좋아요 성공 테스트")
-    public void addLikeToComment() {
+    public void test_AddLikeToComment() {
         //Given
-        //새로운 유저생성
         User newUser = User.builder().id(2L).build();
         UserDetailsImpl userDetails1 = new UserDetailsImpl(newUser);
-//        기존 유저 삭제
-//        given(userRepository.findById(1L)).willReturn(Optional.of(user));
         given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
-
         given(commentLikeRepository.existsByUserAndComment(any(),any())).willReturn(false);
         given(userRepository.findById(2L)).willReturn(Optional.of(newUser));
-        //when
-//        //댓글 생성한 사용자는 좋아요 불가능
-//        commentLikeService.addLikeToComment(1L, 1L, userDetails);
 
+        //When
         commentLikeService.addLikeToComment(1L, 1L, userDetails1);
 
+        //Then
         assertThat(comment.getId()).isEqualTo(1L);
         assertThat(comment.getContent()).isEqualTo("testcontent");
         assertThat(comment.getCount()).isEqualTo(2);
@@ -94,17 +91,63 @@ class CommentLikeServiceImplTest {
 
     @Test
     @DisplayName("댓글에 좋아요 삭제 성공 테스트")
-    public void deleteLikeFromComment(){
+    public void test_DeleteLikeFromComment(){
+        //Given
         User newUser = User.builder().id(2L).build();
         UserDetailsImpl userDetails1 = new UserDetailsImpl(newUser);
         CommentLike commentLike = new CommentLike(userDetails1.getUser(), comment);
         given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
         given(commentLikeRepository.findByCommentAndUser(comment, userDetails1.getUser())).willReturn(Optional.of(commentLike));
 
+        //When
         commentLikeService.deleteLikeFromComment(1L, 1L, userDetails1);
 
+        //Then
         assertThat(comment.getId()).isEqualTo(1L);
         assertThat(comment.getContent()).isEqualTo("testcontent");
         assertThat(comment.getCount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("댓글 존재하지 않는 경우 실패")
+    public void test_CommentNotFound() {
+        //Given
+        given(commentRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // When, Then
+        assertThrows(CommentNotFoundException.class, () -> {
+            commentLikeService.addLikeToComment(1L, 1L, userDetails); // 특정 댓글 ID와 사용자 ID 전달
+        });
+    }
+
+    @Test
+    @DisplayName("좋아요 댓글을 이미 누른 경우 실패 테스트 코드")
+    public void test_AlreadyCommentLike() {
+        // Given
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        //When
+        when(commentLikeRepository.existsByUserAndComment(userDetails.getUser(), comment)).thenReturn(false);
+
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            commentLikeService.addLikeToComment(1L, 1L, userDetails);
+        }, "You have already comment like");
+    }
+
+    @Test
+    @DisplayName("댓글 작성자가 좋아요 누르면 실패하는 테스트 코드")
+    public void test_OwnCommentNotLiked() {
+        //Given
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        //When
+        when(commentLikeRepository.existsByUserAndComment(userDetails.getUser(), comment)).thenReturn(true);
+
+        //Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            commentLikeService.addLikeToComment(1L, 1L, userDetails);
+        }, "You cannot like your own comment");
+
     }
 }
