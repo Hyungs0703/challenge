86dfd40a0e7a3c919732776938e72901a8c834ea
Hyungs0703 <hyungs0703@naver.com>
2024-07-05 17:06:21 +0900

    팔로우 서비스 생성

--- src/main/java/com/twelve/challengeapp/controller/FollowController.java
+++ src/main/java/com/twelve/challengeapp/controller/FollowController.java
@@ -1,8 +1,12 @@
 package com.twelve.challengeapp.controller;
 
+import com.twelve.challengeapp.jwt.UserDetailsImpl;
+import com.twelve.challengeapp.service.FollowServiceImpl;
 import com.twelve.challengeapp.util.SuccessResponseFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/api/users/{userId}/followers")
 public class FollowController {
 
+    private final FollowServiceImpl followService;
 
     @PostMapping
-    public ResponseEntity<?> addFollow() {
+    public ResponseEntity<?> addFollow(@PathVariable Long userId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        followService.addFollow(userId, userDetails);
 
         return SuccessResponseFactory.ok();
     }
---
+++ src/main/java/com/twelve/challengeapp/service/FollowServiceImpl.java
@@ -0,0 +1,11 @@
+package com.twelve.challengeapp.service;
+
+import com.twelve.challengeapp.jwt.UserDetailsImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FollowServiceImpl {
+
+    public void addFollow(Long userId, UserDetailsImpl userDetails) {
+    }
+}
